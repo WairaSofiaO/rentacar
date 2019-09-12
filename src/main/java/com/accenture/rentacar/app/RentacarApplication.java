0fd9bb8e0693347df44f3181bc677bb4a09842e4
Andrea Valderrama <wairasofiaocamporozco@gmail.com>
2019-09-11 20:37:07 -0500

    Clase 11 de sept: utilidades, librerias

--- src/main/java/com/accenture/rentacar/app/RentacarApplication.java
+++ src/main/java/com/accenture/rentacar/app/RentacarApplication.java
@@ -1,13 +1,18 @@
 package com.accenture.rentacar.app;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.accenture.utilidades.fechas.Fecha;
 
 @SpringBootApplication
 public class RentacarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RentacarApplication.class, args);
+		//SpringApplication.run(RentacarApplication.class, args);
+		System.out.println("resultado " + Fecha.calcularDias(null, null));
+		
+		
+	
 	}
 
 }
